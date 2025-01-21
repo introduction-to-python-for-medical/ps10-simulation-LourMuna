e97e09dec5fcbea18fe0e3ad31a11f85d33040fa
LourMuna <lourmuna2019@gmail.com>
2025-01-21 19:42:58 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -1,21 +1,24 @@
 import copy
 
-
-def spread_fire(grid, grid_size):
+def spread_fire(grid):
     """Update the forest grid based on fire spreading rules."""
+    grid_size = len(grid)
     update_grid = copy.deepcopy(grid)
-    for i in range(grid_size):
-        for j in range(grid_size):
-            if grid[i][j] == 1:
+    
+    for i in range(grid_size):  # Iterate over all rows
+        for j in range(grid_size):  # Iterate over all columns
+            if grid[i][j] == 1:  # If the cell contains a tree
                 neighbors = []
-                if i > 0: 
+                
+                if i > 0:  # Check above
                     neighbors.append(grid[i-1][j])
-                if i < grid_size - 1: 
+                if i < grid_size - 1:  # Check below
                     neighbors.append(grid[i+1][j])
-                if j > 0: 
+                if j > 0:  # Check left
                     neighbors.append(grid[i][j-1])
-                if j < grid_size - 1: 
+                if j < grid_size - 1:  # Check right
                     neighbors.append(grid[i][j+1])
+
                 if 2 in neighbors:
                     update_grid[i][j] = 2
 
