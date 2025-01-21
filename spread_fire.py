638192d489c2025b02cf756914dc8cc50d43ed24
LourMuna <lourmuna2019@gmail.com>
2025-01-21 19:32:45 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -9,10 +9,14 @@ def spread_fire(grid, grid_size):
             if grid[i][j] == 1:
                 neighbors = []
                 
-                if i > 0: neighbors.append(grid[i-1][j])
-                if i < grid_size - 1: neighbors.append(grid[i+1][j])
-                if j > 0: neighbors.append(grid[i][j-1])
-                if j < grid_size - 1: neighbors.append(grid[i][j+1])
+                if i > 0: 
+                    neighbors.append(grid[i-1][j])
+                if i < grid_size - 1: 
+                    neighbors.append(grid[i+1][j])
+                if j > 0: 
+                    neighbors.append(grid[i][j-1])
+                if j < grid_size - 1: 
+                    neighbors.append(grid[i][j+1])
 
                 if 2 in neighbors:
                     update_grid[i][j] = 2
